01ae2138304ba314151b55d620cb5bc40595d525
Estoreals <daniel-aguero@uadec.edu.mx>
2024-03-01 15:13:11 -0600

    correcion

--- AccesoDatos/ServicioDeEmail/SupportDeEmail.vb
+++ AccesoDatos/ServicioDeEmail/SupportDeEmail.vb
@@ -2,8 +2,8 @@
     Inherits EnviadorDeCorrecoPrincipal
 
     Public Sub New()
-        senderMail = "" 'AQUI ESCRIBE TU EMAIL
-        password = "" 'Contraseña generada para la aplicacion de correo
+        senderMail = "petpalacemva@gmail.com" 'AQUI ESCRIBE TU EMAIL
+        password = "zhyy ayyh yejo bwyf" 'Contraseña generada para la aplicacion de correo
         host = "smtp.gmail.com" 'tipo de seficio de emial que quieras, outlook,gmail, etc
         port = 587 'el puerto que soporte tu servicio de correo
         ssl = True 'si tiene ssl o no el servicio.
